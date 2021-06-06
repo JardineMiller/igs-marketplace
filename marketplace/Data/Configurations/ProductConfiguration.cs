eafc65faa1efc0e8cd890f264f2cf9d4729c14d3
Jardine <jardine.miller@gmail.com>
2021-06-06 19:48:48 +0100

    Add Product seed data

--- marketplace/Data/Configurations/ProductConfiguration.cs
+++ marketplace/Data/Configurations/ProductConfiguration.cs
@@ -1,3 +1,4 @@
+using System;
 using marketplace.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -27,6 +28,41 @@ namespace marketplace.Data.Configurations
 
             builder.Property(p => p.Price)
                 .HasColumnType("decimal(18,4)");
+
+            builder.HasData(
+                new Product
+                {
+                    Id = 1,
+                    Code = "Product-001",
+                    Name = "Product 1",
+                    CreatedOn = DateTimeOffset.UtcNow,
+                    Price = (decimal) 10.25
+                },
+                new Product
+                {
+                    Id = 2,
+                    Code = "Product-002",
+                    Name = "Product 2",
+                    CreatedOn = DateTimeOffset.UtcNow,
+                    Price = (decimal) 45.00
+                },
+                new Product
+                {
+                    Id = 3,
+                    Code = "Product-003",
+                    Name = "Product 3",
+                    CreatedOn = DateTimeOffset.UtcNow,
+                    Price = (decimal) 9.25
+                },
+                new Product
+                {
+                    Id = 4,
+                    Code = "Product-004",
+                    Name = "Product 4",
+                    CreatedOn = DateTimeOffset.UtcNow,
+                    Price = (decimal) 3.99
+                }
+            );
         }
     }
 }
